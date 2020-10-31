52600d794c1fd7fa6adf92aa5e4cf49878e52add
unknown <chandresh.02vikram@gmail.com>
2020-10-31 16:03:58 +0530

    added  gitDemo project to local repository

---
+++ src/main/java/com/chandreshVikram/app/GitDemo/App.java
@@ -0,0 +1,19 @@
+package com.chandreshVikram.app.GitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	
+	public void addData(String Data)
+	{
+		System.out.println(" string data is:: " + Data);
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
