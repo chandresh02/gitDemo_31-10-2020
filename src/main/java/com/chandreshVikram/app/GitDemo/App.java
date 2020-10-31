fdb31012b47a166689f59251fb5d8fa0b31f7fd2
unknown <chandresh.02vikram@gmail.com>
2020-10-31 16:41:13 +0530

    added constant along with delete comment

--- src/main/java/com/chandreshVikram/app/GitDemo/App.java
+++ src/main/java/com/chandreshVikram/app/GitDemo/App.java
@@ -1,23 +1,15 @@
 package com.chandreshVikram.app.GitDemo;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-	
-	
-	public void addData(String Data)
-	{
-		System.out.println(" string data is:: " + Data);
-		if(Data.equals("chandresh"))
-		{
-			System.out.println(" welcome to git chandresh vikram");
+public class App {
+
+	public void addData(String Data) {
+
+		if (Data.equals(Constant.DATA_TYPE)) {
+			System.out.println(" welcome to git chandresh vikram ");
 		}
 	}
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
---
+++ src/main/java/com/chandreshVikram/app/GitDemo/Constant.java
@@ -0,0 +1,6 @@
+package com.chandreshVikram.app.GitDemo;
+
+public class Constant {
+ 
+	public static String DATA_TYPE="Chandresh Vikram";
+}
